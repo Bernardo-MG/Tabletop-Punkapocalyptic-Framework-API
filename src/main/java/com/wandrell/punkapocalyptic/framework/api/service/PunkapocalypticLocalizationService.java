a3e0dad89fea2513f1c1c5056c229d3aeb540d2c
Bernardo Martínez Garrido <programming@wandrell.com>
2014-08-29 22:29:54 +0200

    Added a method to localize attributes.

--- src/main/java/com/wandrell/punkapocalyptic/framework/api/service/PunkapocalypticLocalizationService.java
+++ src/main/java/com/wandrell/punkapocalyptic/framework/api/service/PunkapocalypticLocalizationService.java
@@ -2,6 +2,8 @@ package com.wandrell.punkapocalyptic.framework.api.service;
 
 public interface PunkapocalypticLocalizationService {
 
+    public String getAttributeString(final String attribute);
+
     public String getLabelString(final String key);
 
     public String getUnitNameString(final String unit);
