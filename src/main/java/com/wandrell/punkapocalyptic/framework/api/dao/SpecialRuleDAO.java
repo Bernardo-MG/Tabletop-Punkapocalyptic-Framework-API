fef6be9a56dba88b2bc687c50ca6439444b7dc95
Bernardo Martínez Garrido <programming@wandrell.com>
2014-08-29 14:09:49 +0200

    Changed the special rules DAO.

--- src/main/java/com/wandrell/punkapocalyptic/framework/api/dao/SpecialRuleDAO.java
+++ src/main/java/com/wandrell/punkapocalyptic/framework/api/dao/SpecialRuleDAO.java
@@ -1,11 +1,9 @@
 package com.wandrell.punkapocalyptic.framework.api.dao;
 
-import java.util.Map;
-
 import com.wandrell.tabletop.punkapocalyptic.rule.SpecialRule;
 
 public interface SpecialRuleDAO {
 
-    public Map<String, SpecialRule> getSpecialRulesMap();
+    public SpecialRule getSpecialRule(final String rule);
 
 }
