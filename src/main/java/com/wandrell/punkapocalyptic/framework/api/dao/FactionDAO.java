30d68ea91c3d856806eb12581f515d00bcbfeb60
Bernardo Martínez Garrido <programming@wandrell.com>
2014-08-29 14:44:30 +0200

    Removed unneeded method from the faction and unit DAOs.

--- src/main/java/com/wandrell/punkapocalyptic/framework/api/dao/FactionDAO.java
+++ src/main/java/com/wandrell/punkapocalyptic/framework/api/dao/FactionDAO.java
@@ -1,15 +1,9 @@
 package com.wandrell.punkapocalyptic.framework.api.dao;
 
-import java.util.Collection;
-
 import com.wandrell.tabletop.punkapocalyptic.faction.Faction;
 
 public interface FactionDAO {
 
     public Faction getFaction(final String faction);
-
-    public Collection<String> getFactionNames();
-
-    public Collection<String> getUnits(final String faction);
 
 }
--- src/main/java/com/wandrell/punkapocalyptic/framework/api/dao/UnitDAO.java
+++ src/main/java/com/wandrell/punkapocalyptic/framework/api/dao/UnitDAO.java
@@ -1,11 +1,7 @@
 package com.wandrell.punkapocalyptic.framework.api.dao;
 
 import java.util.Collection;
-import java.util.Map;
 
-import com.wandrell.tabletop.interval.Interval;
-import com.wandrell.tabletop.punkapocalyptic.inventory.Armor;
-import com.wandrell.tabletop.punkapocalyptic.inventory.Weapon;
 import com.wandrell.tabletop.punkapocalyptic.unit.AvailabilityUnit;
 import com.wandrell.tabletop.punkapocalyptic.unit.Unit;
 
@@ -13,12 +9,6 @@ public interface UnitDAO {
 
     public AvailabilityUnit getAvailabilityUnit(final String unit);
 
-    public Collection<Armor> getAvailableArmors(final String unit);
-
-    public Collection<Weapon> getAvailableWeapons(final String unit);
-
-    public Map<String, Unit> getUnits();
-
-    public Interval getWeaponsInterval(final String unit);
+    public Collection<Unit> getUnits();
 
 }
