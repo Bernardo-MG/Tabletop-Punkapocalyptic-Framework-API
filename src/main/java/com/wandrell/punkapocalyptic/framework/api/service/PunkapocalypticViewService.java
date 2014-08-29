4bfc2591c9829517392ebe52e28b54a54d6e9755
Bernardo Martínez Garrido <programming@wandrell.com>
2014-08-29 18:34:06 +0200

    Removed unneeded services.

--- src/main/java/com/wandrell/punkapocalyptic/framework/api/service/PunkapocalypticViewService.java
+++
@@ -1,9 +0,0 @@
-package com.wandrell.punkapocalyptic.framework.api.service;
-
-import javax.swing.JFrame;
-
-public interface PunkapocalypticViewService {
-
-    public void initializeMainFrame(final JFrame frame);
-
-}
