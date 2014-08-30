8fcffe71a88cdc931a075a9fe991eadf6a857ccb
Bernardo Martínez Garrido <programming@wandrell.com>
2014-08-30 20:10:23 +0200

    Added a method for localizing armor names.

--- src/main/java/com/wandrell/punkapocalyptic/framework/api/service/PunkapocalypticLocalizationService.java
+++ src/main/java/com/wandrell/punkapocalyptic/framework/api/service/PunkapocalypticLocalizationService.java
@@ -2,6 +2,8 @@ package com.wandrell.punkapocalyptic.framework.api.service;
 
 public interface PunkapocalypticLocalizationService {
 
+    public String getArmorString(final String armor);
+
     public String getAttributeString(final String attribute);
 
     public String getLabelString(final String key);
