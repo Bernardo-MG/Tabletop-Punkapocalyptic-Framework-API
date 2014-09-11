162a1ad1d05c7a62ad18280efa97494d3d9eeeae
Bernardo Martínez Garrido <programming@wandrell.com>
2014-09-11 17:09:47 +0200

    Added a service for files.

---
+++ src/main/java/com/wandrell/tabletop/business/service/punkapocalyptic/PunkapocalypticFileService.java
@@ -0,0 +1,9 @@
+package com.wandrell.tabletop.business.service.punkapocalyptic;
+
+import com.wandrell.tabletop.business.model.punkapocalyptic.unit.Band;
+
+public interface PunkapocalypticFileService {
+
+    public void saveBand(final Band band);
+
+}
