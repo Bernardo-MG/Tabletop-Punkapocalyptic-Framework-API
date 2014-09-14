979ea5b14ede104bd358790ad2d7c118d07823ab
Bernardo Martínez Garrido <programming@wandrell.com>
2014-09-14 13:46:13 +0200

    Added a new listener.

---
+++ src/main/java/com/wandrell/tabletop/business/procedure/punkapocalyptic/event/UnitConfigurationListener.java
@@ -0,0 +1,11 @@
+package com.wandrell.tabletop.business.procedure.punkapocalyptic.event;
+
+import java.util.EventListener;
+
+import com.wandrell.tabletop.business.model.punkapocalyptic.unit.event.UnitEvent;
+
+public interface UnitConfigurationListener extends EventListener {
+
+    public void unitSelected(final UnitEvent e);
+
+}
