cf392777a9db589419ce52b39d4cc78c586c5289
Bernardo Martínez Garrido <programming@wandrell.com>
2014-09-15 22:45:58 +0200

    The army builder controller handles the maximum allowed units.

--- src/main/java/com/wandrell/tabletop/business/procedure/punkapocalyptic/ArmyBuilderController.java
+++ src/main/java/com/wandrell/tabletop/business/procedure/punkapocalyptic/ArmyBuilderController.java
@@ -1,11 +1,14 @@
 package com.wandrell.tabletop.business.procedure.punkapocalyptic;
 
 import com.wandrell.tabletop.business.model.punkapocalyptic.unit.Gang;
+import com.wandrell.tabletop.business.model.valuehandler.ValueHandler;
 import com.wandrell.tabletop.business.procedure.ProcedureValidator;
 
 public interface ArmyBuilderController extends ProcedureValidator {
 
     public Gang getGang();
+
+    public ValueHandler getMaxUnits();
 
     public UnitConfigurationController getUnitConfigurationController();
 
