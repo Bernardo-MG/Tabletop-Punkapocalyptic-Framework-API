b08ea31ff3d72f060bf7b326af98c6732b8bf106
Bernardo Martínez Garrido <programming@wandrell.com>
2014-09-15 20:20:19 +0200

    Added new methods.

--- src/main/java/com/wandrell/tabletop/business/service/punkapocalyptic/RulesetService.java
+++ src/main/java/com/wandrell/tabletop/business/service/punkapocalyptic/RulesetService.java
@@ -1,7 +1,16 @@
 package com.wandrell.tabletop.business.service.punkapocalyptic;
 
+import com.wandrell.tabletop.business.model.punkapocalyptic.unit.Gang;
+import com.wandrell.tabletop.business.model.punkapocalyptic.unit.Unit;
+
 public interface RulesetService {
 
     public Integer getBulletCost();
+
+    public Integer getGangValoration(final Gang gang);
+
+    public Integer getMaxAllowedUnits(final Gang gang);
+
+    public Integer getUnitValoration(final Unit unit);
 
 }
