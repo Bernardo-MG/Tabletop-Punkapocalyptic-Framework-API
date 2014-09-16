ea3add78edf18fb6f7203a82c0c029e6181c0013
Bernardo Martínez Garrido <programming@wandrell.com>
2014-09-16 09:24:15 +0200

    Added special rules names class.

---
+++ src/main/java/com/wandrell/tabletop/business/conf/SpecialRuleNameConf.java
@@ -0,0 +1,11 @@
+package com.wandrell.tabletop.business.conf;
+
+public final class SpecialRuleNameConf {
+
+    public static final String TWO_HANDED = "two-handed";
+
+    private SpecialRuleNameConf() {
+        super();
+    }
+
+}
