a0826c465f9b311ff520027718773df5ce1ce68f
Bernardo Martínez Garrido <programming@wandrell.com>
2014-09-21 02:12:00 +0200

    Adapted to the latest library changes.

---
+++ src/main/java/com/wandrell/tabletop/business/conf/WeaponNameConf.java
@@ -0,0 +1,12 @@
+package com.wandrell.tabletop.business.conf;
+
+public final class WeaponNameConf {
+
+    public static final String IMPROVISED_WEAPON = "improvised_weapon";
+    public static final String LIGHT_MACE        = "light_mace";
+
+    private WeaponNameConf() {
+        super();
+    }
+
+}
--- src/main/java/com/wandrell/tabletop/data/persistence/punkapocalyptic/RulesetDAO.java
+++ src/main/java/com/wandrell/tabletop/data/persistence/punkapocalyptic/RulesetDAO.java
@@ -1,13 +1,13 @@
 package com.wandrell.tabletop.data.persistence.punkapocalyptic;
 
-import com.wandrell.tabletop.business.model.punkapocalyptic.ruleset.ArmyBuilderUnitConstraint;
-import com.wandrell.tabletop.business.model.punkapocalyptic.ruleset.SpecialRule;
+import com.wandrell.tabletop.business.model.punkapocalyptic.ruleset.constraint.GangConstraint;
+import com.wandrell.tabletop.business.model.punkapocalyptic.ruleset.specialrule.SpecialRule;
 
 public interface RulesetDAO {
 
     public SpecialRule getSpecialRule(final String rule);
 
-    public ArmyBuilderUnitConstraint getUnitConstraint(final String constraint,
+    public GangConstraint getUnitConstraint(final String constraint,
             final String unit);
 
 }
