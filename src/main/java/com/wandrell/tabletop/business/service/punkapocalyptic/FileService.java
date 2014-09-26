bd5de2ecb35c8b31af353d45b767f31b541fbd20
Bernardo Martínez Garrido <programming@wandrell.com>
2014-09-26 10:11:08 +0200

    Adapted the file service for showing a file chooser.

--- src/main/java/com/wandrell/tabletop/business/service/punkapocalyptic/FileService.java
+++ src/main/java/com/wandrell/tabletop/business/service/punkapocalyptic/FileService.java
@@ -1,9 +1,11 @@
 package com.wandrell.tabletop.business.service.punkapocalyptic;
 
+import java.awt.Component;
+
 import com.wandrell.tabletop.business.model.punkapocalyptic.unit.Gang;
 
 public interface FileService {
 
-    public void saveGang(final Gang gang);
+    public void saveGang(final Gang gang, final Component parent);
 
 }
