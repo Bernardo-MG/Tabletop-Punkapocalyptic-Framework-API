3220a3df25743353dde0ec8b6d0effb30a5bd630
Bernardo Martínez Garrido <programming@wandrell.com>
2014-10-23 16:00:30 +0200

    Removed the availability unit. Adapted to the new armor availability class.

--- src/main/java/com/wandrell/tabletop/data/service/punkapocalyptic/model/DataModelService.java
+++ src/main/java/com/wandrell/tabletop/data/service/punkapocalyptic/model/DataModelService.java
@@ -2,22 +2,26 @@ package com.wandrell.tabletop.data.service.punkapocalyptic.model;
 
 import java.util.Collection;
 
-import com.wandrell.tabletop.business.model.punkapocalyptic.AvailabilityUnit;
 import com.wandrell.tabletop.business.model.punkapocalyptic.availability.UnitArmorAvailability;
+import com.wandrell.tabletop.business.model.punkapocalyptic.availability.UnitEquipmentAvailability;
 import com.wandrell.tabletop.business.model.punkapocalyptic.availability.UnitWeaponAvailability;
 import com.wandrell.tabletop.business.model.punkapocalyptic.faction.Faction;
 import com.wandrell.tabletop.business.model.punkapocalyptic.ruleset.constraint.UnitGangConstraint;
+import com.wandrell.tabletop.business.model.punkapocalyptic.unit.Unit;
 
 public interface DataModelService {
 
     public Collection<Faction> getAllFactions();
 
-    public Collection<AvailabilityUnit> getFactionUnits(final String faction);
+    public Collection<Unit> getFactionUnits(final String faction);
 
     public UnitArmorAvailability getUnitArmorAvailability(final String unit);
 
     public Collection<UnitGangConstraint> getUnitConstraints(final String unit,
             final String faction);
+
+    public UnitEquipmentAvailability getUnitEquipmentAvailability(
+            final String unit);
 
     public UnitWeaponAvailability getUnitWeaponAvailability(final String unit);
 
