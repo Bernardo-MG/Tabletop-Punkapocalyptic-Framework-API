3038a20259ce04d87d2a4feaa5e2783909fc4f6d
Bernardo Martínez Garrido <programming@wandrell.com>
2014-11-03 11:22:40 +0100

    Removed the unit configuration listener.

--- src/main/java/com/wandrell/tabletop/business/procedure/punkapocalyptic/UnitConfigurationController.java
+++ src/main/java/com/wandrell/tabletop/business/procedure/punkapocalyptic/UnitConfigurationController.java
@@ -2,17 +2,10 @@ package com.wandrell.tabletop.business.procedure.punkapocalyptic;
 
 import com.wandrell.tabletop.business.model.punkapocalyptic.unit.Unit;
 import com.wandrell.tabletop.business.procedure.ProcedureValidator;
-import com.wandrell.tabletop.business.procedure.punkapocalyptic.event.UnitConfigurationListener;
 
 public interface UnitConfigurationController extends ProcedureValidator {
 
-    public void addUnitConfigurationListener(
-            final UnitConfigurationListener listener);
-
     public Unit getUnit();
-
-    public void removeUnitConfigurationListener(
-            final UnitConfigurationListener listener);
 
     public void setUnit(final Unit unit);
 
