1ab54dabfdcab6caf876425197ce390811fbfd1b
Bernardo Martínez Garrido <programming@wandrell.com>
2014-11-03 11:27:05 +0100

    Removed unused localization methods.

--- src/main/java/com/wandrell/tabletop/business/service/punkapocalyptic/LocalizationService.java
+++ src/main/java/com/wandrell/tabletop/business/service/punkapocalyptic/LocalizationService.java
@@ -4,17 +4,11 @@ public interface LocalizationService {
 
     public String getArmorString(final String armor);
 
-    public String getAttributeString(final String attribute);
-
-    public String getLabelString(final String key);
-
     public String getMessageString(final String message);
 
     public String getSpecialRuleString(final String name);
 
     public String getUnitNameString(final String unit);
-
-    public String getWeaponAttributeString(final String attribute);
 
     public String getWeaponString(final String weapon);
 
