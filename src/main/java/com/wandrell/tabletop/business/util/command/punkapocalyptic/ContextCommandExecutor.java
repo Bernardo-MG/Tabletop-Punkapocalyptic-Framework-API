a8afe4549d12d0f8099e47f3636503a8ee5262c0
Bernardo Martínez Garrido <programming@wandrell.com>
2014-11-05 09:15:33 +0100

    Added an interface for the context command executor.

---
+++ src/main/java/com/wandrell/tabletop/business/util/command/punkapocalyptic/ContextCommandExecutor.java
@@ -0,0 +1,22 @@
+package com.wandrell.tabletop.business.util.command.punkapocalyptic;
+
+import com.wandrell.service.application.ApplicationInfoService;
+import com.wandrell.tabletop.business.service.punkapocalyptic.FileService;
+import com.wandrell.tabletop.business.service.punkapocalyptic.LocalizationService;
+import com.wandrell.tabletop.business.service.punkapocalyptic.RulesetService;
+import com.wandrell.tabletop.data.service.punkapocalyptic.model.DataModelService;
+import com.wandrell.util.command.CommandExecutor;
+
+public interface ContextCommandExecutor extends CommandExecutor {
+
+    public void setApplicationInfoService(final ApplicationInfoService service);
+
+    public void setDataModelService(final DataModelService service);
+
+    public void setFileService(final FileService service);
+
+    public void setLocalizationService(final LocalizationService service);
+
+    public void setRulesetService(final RulesetService service);
+
+}
