54bd3969a85c1bf29f384e5090cf9bece13c472c
Bernardo Martínez Garrido <programming@wandrell.com>
2015-01-02 22:40:27 +0100

    Added a method to acquire the file where the gang report is to be saved.

--- src/main/java/com/wandrell/tabletop/business/service/punkapocalyptic/FileService.java
+++ src/main/java/com/wandrell/tabletop/business/service/punkapocalyptic/FileService.java
@@ -2,9 +2,13 @@ package com.wandrell.tabletop.business.service.punkapocalyptic;
 
 import java.io.File;
 
+import javafx.stage.Stage;
+
 import com.wandrell.tabletop.business.model.punkapocalyptic.unit.Gang;
 
 public interface FileService {
+
+    public File getFileToPrintGangOnDesktop(final Stage stage);
 
     public String getTitleImagePath();
 
