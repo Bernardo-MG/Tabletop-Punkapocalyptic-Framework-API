e5fd2dd07f353002f4249cc4f9d6e44037014d0f
Bernardo Martínez Garrido <programming@wandrell.com>
2015-02-07 23:14:53 +0100

    Updated deployment script.

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -2,7 +2,7 @@
 # This script deploys the application using the POM cofiguration
 # It is triggered only commits to the master or develop branches. Pulls are ignored
 
-if [ "$TRAVIS_PULL_REQUEST" == "false" ] && [[ "$TRAVIS_BRANCH" == "master" ] || [ "$TRAVIS_BRANCH" == "develop" ]]; then
+if [ "$TRAVIS_PULL_REQUEST" == "false" ] && [[ "$TRAVIS_BRANCH" == "master" || "$TRAVIS_BRANCH" == "develop" ]]; then
 
    mvn deploy --settings ~/settings.xml
 
