f136ac133960a69882ffecd49cee57bb5df8fc3f
Bernardo Martínez Garrido <programming@wandrell.com>
2015-03-18 17:22:56 +0100

    Localization service no longer localizes model classes names.

--- src/main/java/com/wandrell/tabletop/punkapocalyptic/service/LocalizationService.java
+++ src/main/java/com/wandrell/tabletop/punkapocalyptic/service/LocalizationService.java
@@ -2,26 +2,10 @@ package com.wandrell.tabletop.punkapocalyptic.service;
 
 public interface LocalizationService {
 
-    public String getArmorString(final String armor);
-
-    public String getEquipmentString(final String equipment);
-
-    public String getFactionNameString(final String faction);
-
     public String getMessageString(final String message);
-
-    public String getMutationString(final String mutation);
 
     public String getReportString(final String key);
 
-    public String getSpecialRuleString(final String name);
-
-    public String getUnitNameString(final String unit);
-
     public String getViewString(final String key);
-
-    public String getWeaponEnhancementString(final String enhancement);
-
-    public String getWeaponString(final String weapon);
 
 }
