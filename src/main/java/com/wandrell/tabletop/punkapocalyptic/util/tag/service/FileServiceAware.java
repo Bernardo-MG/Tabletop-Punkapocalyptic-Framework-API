2fdb24e561de8642a452595859340b860e4c4eb8
Bernardo Martínez Garrido <programming@wandrell.com>
2015-03-23 21:07:13 +0100

    Removed Java 8 dependencies.

--- src/main/java/com/wandrell/tabletop/punkapocalyptic/util/tag/service/FileServiceAware.java
+++
@@ -1,9 +0,0 @@
-package com.wandrell.tabletop.punkapocalyptic.util.tag.service;
-
-import com.wandrell.tabletop.punkapocalyptic.service.FileService;
-
-public interface FileServiceAware {
-
-    public void setFileService(final FileService service);
-
-}
