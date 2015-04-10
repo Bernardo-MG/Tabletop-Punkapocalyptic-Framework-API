fa39c40d777d6fe5a3668c95ca32ff33860c6c8b
Bernardo Martínez Garrido <programming@wandrell.com>
2015-04-11 00:28:37 +0200

    Adapted to the latest version of the Java Patterns library.

--- src/main/java/com/wandrell/tabletop/punkapocalyptic/util/tag/repository/WeaponRepositoryAware.java
+++ src/main/java/com/wandrell/tabletop/punkapocalyptic/util/tag/repository/WeaponRepositoryAware.java
@@ -1,10 +1,12 @@
 package com.wandrell.tabletop.punkapocalyptic.util.tag.repository;
 
-import com.wandrell.pattern.repository.Repository;
+import com.google.common.base.Predicate;
+import com.wandrell.pattern.repository.QueryableRepository;
 import com.wandrell.tabletop.punkapocalyptic.model.inventory.Weapon;
 
 public interface WeaponRepositoryAware {
 
-    public void setWeaponRepository(final Repository<Weapon> repository);
+    public void setWeaponRepository(
+            final QueryableRepository<Weapon, Predicate<Weapon>> repository);
 
 }
