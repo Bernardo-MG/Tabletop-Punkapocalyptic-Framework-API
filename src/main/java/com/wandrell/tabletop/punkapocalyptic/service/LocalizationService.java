c155f1ee241492786202daa0f2ed4273aedfccfb
Bernardo Martínez Garrido <programming@wandrell.com>
2015-04-12 18:35:33 +0200

    Corrected a typo on a parameter name.

--- src/main/java/com/wandrell/tabletop/punkapocalyptic/service/LocalizationService.java
+++ src/main/java/com/wandrell/tabletop/punkapocalyptic/service/LocalizationService.java
@@ -2,7 +2,7 @@ package com.wandrell.tabletop.punkapocalyptic.service;
 
 public interface LocalizationService {
 
-    public String getMessageString(final String message);
+    public String getMessageString(final String key);
 
     public String getReportString(final String key);
 
