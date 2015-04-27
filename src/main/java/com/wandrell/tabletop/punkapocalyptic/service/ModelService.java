764ae1b659cc7a50fde26480979859946040eef2
Bernardo Martínez Garrido <programming@wandrell.com>
2015-04-27 21:40:42 +0200

    The ranged weapons firearm status can no longer be set.

--- src/main/java/com/wandrell/tabletop/punkapocalyptic/service/ModelService.java
+++ src/main/java/com/wandrell/tabletop/punkapocalyptic/service/ModelService.java
@@ -56,7 +56,7 @@ public interface ModelService {
             final Integer cost, final Collection<SpecialRule> rules,
             final RangedValue penetration, final RangedValue strength,
             final RangedValue distanceCM, final RangedValue distanceInches,
-            final MeleeWeapon weaponMelee);
+            final Boolean firearm, final MeleeWeapon weaponMelee);
 
     public SpecialRule getSpecialRule(final String name);
 
