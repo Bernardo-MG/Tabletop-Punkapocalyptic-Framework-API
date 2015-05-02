487fe3d81d58f913657a9ac52b680659e95a1746
Bernardo Martínez Garrido <programming@wandrell.com>
2015-05-02 21:32:27 +0200

    Added new methods to repositories.

--- src/main/java/com/wandrell/tabletop/punkapocalyptic/repository/FactionUnitAvailabilityRepository.java
+++ src/main/java/com/wandrell/tabletop/punkapocalyptic/repository/FactionUnitAvailabilityRepository.java
@@ -4,6 +4,7 @@ import java.util.Collection;
 
 import com.wandrell.pattern.repository.Repository;
 import com.wandrell.tabletop.punkapocalyptic.model.availability.FactionUnitAvailability;
+import com.wandrell.tabletop.punkapocalyptic.model.unit.UnitTemplate;
 
 public interface FactionUnitAvailabilityRepository extends
         Repository<FactionUnitAvailability> {
@@ -12,5 +13,7 @@ public interface FactionUnitAvailabilityRepository extends
             final String faction);
 
     public FactionUnitAvailability getAvailabilityForUnit(final String unit);
+
+    public Collection<UnitTemplate> getUnitsForFaction(final String faction);
 
 }
--- src/main/java/com/wandrell/tabletop/punkapocalyptic/repository/UnitWeaponAvailabilityRepository.java
+++ src/main/java/com/wandrell/tabletop/punkapocalyptic/repository/UnitWeaponAvailabilityRepository.java
@@ -1,11 +1,20 @@
 package com.wandrell.tabletop.punkapocalyptic.repository;
 
+import java.util.Collection;
+
 import com.wandrell.pattern.repository.Repository;
 import com.wandrell.tabletop.punkapocalyptic.model.availability.UnitWeaponAvailability;
+import com.wandrell.tabletop.punkapocalyptic.model.inventory.Weapon;
+import com.wandrell.tabletop.punkapocalyptic.model.inventory.WeaponEnhancement;
 
 public interface UnitWeaponAvailabilityRepository extends
         Repository<UnitWeaponAvailability> {
 
     public UnitWeaponAvailability getAvailabilityForUnit(final String unit);
+
+    public Collection<Weapon> getAvailableWeaponsForUnit(final String unit);
+
+    public Collection<WeaponEnhancement> getEnhancementsForUnitAndWeapon(
+            final String unit, final String weapon);
 
 }
