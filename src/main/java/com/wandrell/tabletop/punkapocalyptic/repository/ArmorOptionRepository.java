12a60e0215b5a06aa82c6c1c7ab294ba06bf24c6
Bernardo Martínez Garrido <programming@wandrell.com>
2015-05-10 12:55:38 +0200

    Removed unneeded repositories.

--- src/main/java/com/wandrell/tabletop/punkapocalyptic/repository/ArmorOptionRepository.java
+++
@@ -1,8 +0,0 @@
-package com.wandrell.tabletop.punkapocalyptic.repository;
-
-import com.wandrell.pattern.repository.Repository;
-import com.wandrell.tabletop.punkapocalyptic.model.availability.option.ArmorOption;
-
-public interface ArmorOptionRepository extends Repository<ArmorOption> {
-
-}
