b019dcd44edb523ff82643df457e99eecae43c1c
Bernardo Martínez Garrido <programming@wandrell.com>
2015-05-19 23:25:04 +0200

    Adapted to the new stats library.

--- src/main/java/com/wandrell/tabletop/punkapocalyptic/procedure/event/GangBuilderStatusChangedListener.java
+++ src/main/java/com/wandrell/tabletop/punkapocalyptic/procedure/event/GangBuilderStatusChangedListener.java
@@ -2,7 +2,7 @@ package com.wandrell.tabletop.punkapocalyptic.procedure.event;
 
 import java.util.EventListener;
 
-import com.wandrell.tabletop.event.ValueChangeEvent;
+import com.wandrell.tabletop.stat.event.ValueChangeEvent;
 
 public interface GangBuilderStatusChangedListener extends EventListener,
         GangChangedListener, UnitChangedListener {
