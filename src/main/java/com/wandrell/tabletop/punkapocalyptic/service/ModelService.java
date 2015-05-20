29957cc2c0159d1764c2da3ed00aff45d1a7e2a5
Bernardo Martínez Garrido <programming@wandrell.com>
2015-05-20 23:29:16 +0200

    Added a method to create constraints.

--- src/main/java/com/wandrell/tabletop/punkapocalyptic/service/ModelService.java
+++ src/main/java/com/wandrell/tabletop/punkapocalyptic/service/ModelService.java
@@ -1,11 +1,17 @@
 package com.wandrell.tabletop.punkapocalyptic.service;
 
+import java.util.List;
+
+import com.wandrell.tabletop.procedure.Constraint;
 import com.wandrell.tabletop.punkapocalyptic.model.faction.Faction;
 import com.wandrell.tabletop.punkapocalyptic.model.unit.Gang;
 import com.wandrell.tabletop.punkapocalyptic.model.unit.Unit;
 import com.wandrell.tabletop.punkapocalyptic.model.unit.UnitTemplate;
 
 public interface ModelService {
+
+    public Constraint getConstraint(final Gang gang, final String name,
+            final String unit, final List<String> context);
 
     public Gang getGang(final Faction faction);
 
